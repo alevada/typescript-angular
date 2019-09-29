5110bc9dc861fd0e689e5af5c69e1415a9b00f56
Ale Barina <vadasanmaria@gmail.com>
2019-09-29 16:43:10 +0300

    'snippets'

---
+++ snippet.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello World';
+var is_beginner = false;
+var sentence = "My name is gabriel. \nMy message to you is " + message;
+var sentence2 = "My name is gabriel. \nMy message to you is " + message;
+var sentence2 = "My name is gabriel. My message to you is " + message;
+var values = [1, 2, 3, 4, null];
+console.log(values);
+/*
+function greeter (name) {
+    return 'Hello ' + name;
+}
+
+var g = greeter ('gabriel');
+console.log(g);
+
+*/ 
---
+++ snippet.ts
@@ -0,0 +1,25 @@
+export {}
+
+let message = 'Hello World';
+let is_beginner: boolean = false;
+let sentence: string = `My name is gabriel. 
+My message to you is ${message}`;
+
+let sentence2: string = `My name is gabriel. 
+My message to you is ` + message;
+
+let sentence2: string = "My name is gabriel. My message to you is " + message;
+
+let values: number[] = [1,2,3,4, null];
+
+console.log(values);
+
+/*
+function greeter (name) {
+	return 'Hello ' + name;
+}
+
+var g = greeter ('gabriel');
+console.log(g);
+
+*/

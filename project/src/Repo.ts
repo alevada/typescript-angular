82384fb156afae48a2efaf575ea508f187b28b2f
Ale Barina <vadasanmaria@gmail.com>
2019-10-13 19:02:50 +0300

    testing

--- project/src/Repo.ts
+++ project/src/Repo.ts
@@ -9,7 +9,7 @@ export class Repo {
         this.name           = repo.name;
         this.description    = repo.description;
         this.url            = repo.html_url;
-        this.size           = repo.size;
+        this.size           = repo.size; 
         this.forkCount      = repo.forks;
 
     }
